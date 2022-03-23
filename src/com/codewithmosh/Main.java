c369bb0ba78b52abc13a2b2a8c520e79720d4e5d
Volentis <alirezamovahedifar@gmail.com>
2022-03-23 15:57:13 +0430

    first commit

---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,15 @@
+package com.codewithmosh;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        double x = 1.1;
+        double y = x + 2;
+        System.out.println(y);
+
+    }
+}
+
